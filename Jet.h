a1e80b59924e39e403e7a64ede5744ed2684e17b
Myra Mullis <51244137+mmullis7@users.noreply.github.com>
2019-10-02 17:05:12 -0400

    Defined the Jet class in Jet.h

---
+++ Jet.h
@@ -0,0 +1,24 @@
+//Myra Mullis
+//Jet.h
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle
+{
+	private:
+		int numberOfEngines;
+
+	public:
+		Jet();	
+		explicit Jet(string brand, string model, string fuelType, int numEng=1);
+		virtual ~Jet();
+		int getNumEngines();
+    		void setNumEngines(int numEng);
+    		virtual double mileageEstimate(double time);
+    		virtual string toString();
+}
+
+#endif //DRIVINGSIMULATOR_JET_H
