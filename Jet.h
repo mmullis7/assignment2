bdca40516db143c6b54780221f88b3561713979a
Myra Mullis <51244137+mmullis7@users.noreply.github.com>
2019-10-02 19:56:35 -0400

    Fixed error in Jet.h

--- Jet.h
+++ Jet.h
@@ -11,14 +11,13 @@ class Jet : public PoweredVehicle
 	private:
 		int numberOfEngines;
 
-	public:
-		Jet();	
+	public:	
 		explicit Jet(string brand, string model, string fuelType, int numEng=1);
 		virtual ~Jet();
 		int getNumEngines();
     		void setNumEngines(int numEng);
     		virtual double mileageEstimate(double time);
     		virtual string toString();
-}
+};
 
 #endif //DRIVINGSIMULATOR_JET_H
